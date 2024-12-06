6557bb23bf9f7e29772f83474e65dc87bd1ac6c0
dorjidechen19 <dorjidechen19@gmail.com>
2024-12-06 09:21:29 +0600

    ET-05: implemented login api

--- src/main/java/com/java/eventtracker/users/model/Users.java
+++ src/main/java/com/java/eventtracker/users/model/Users.java
@@ -31,4 +31,44 @@ public class Users {
     private String password;
 
     private String roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public @NotBlank(message = "Username is required and cannot be empty") String getUsername() {
+        return username;
+    }
+
+    public void setUsername(@NotBlank(message = "Username is required and cannot be empty") String username) {
+        this.username = username;
+    }
+
+    public @NotBlank(message = "Email is required and cannot be empty") @Email(message = "Invalid email format") String getEmail() {
+        return email;
+    }
+
+    public void setEmail(@NotBlank(message = "Email is required and cannot be empty") @Email(message = "Invalid email format") String email) {
+        this.email = email;
+    }
+
+    public @NotBlank(message = "Password is required and cannot be empty") String getPassword() {
+        return password;
+    }
+
+    public void setPassword(@NotBlank(message = "Password is required and cannot be empty") String password) {
+        this.password = password;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
 }
