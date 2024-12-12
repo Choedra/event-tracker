0cd45737b4d6c3962c11fe1377036dcd510923cb
Choedra <choedra999@gmail.com>
2024-12-12 14:59:28 +0600

    ET-09: Implement the refresh token generation.

--- src/main/java/com/java/eventtracker/Auth/controller/AuthenticationController.java
+++ src/main/java/com/java/eventtracker/Auth/controller/AuthenticationController.java
@@ -2,7 +2,7 @@ package com.java.eventtracker.Auth.controller;
 
 
 import com.java.eventtracker.Auth.model.AuthRequest;
-import com.java.eventtracker.Auth.service.LoginService;
+import com.java.eventtracker.Auth.service.AuthenticationService;
 import com.java.eventtracker.utils.RestHelper;
 import com.java.eventtracker.utils.RestResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,19 +15,18 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("api/v1/login")
-public class LoginController {
+public class AuthenticationController {
     @Autowired
-    private LoginService loginService;
+    private AuthenticationService loginService;
     /**
      * Handles the authentication for the user provided credentials.
      *
      * @param authRequest The authentication credentials containing object
      * @return The access keys and refresh keys for the associated authenticated user.
      */
-    @PostMapping
+    @PostMapping("/login")
     public ResponseEntity<RestResponse> login(@RequestBody AuthRequest authRequest) {
-        HashMap<String, Object> listHashMap = new HashMap<>();
-        listHashMap.put("access_token", loginService.authenticate(authRequest));
+        HashMap<String, Object> listHashMap = new HashMap<>(loginService.authenticate(authRequest));
         return RestHelper.responseSuccess(listHashMap);
     }
 }
